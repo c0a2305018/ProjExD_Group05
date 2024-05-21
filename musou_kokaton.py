7659030f1d01f53ace900a6a4ed54608e8ca3885
Futa <c0a2305018@edu.teu.ac.jp>
2024-05-21 18:05:05 +0900

    boss追加

--- musou_kokaton.py
+++ musou_kokaton.py
@@ -71,7 +71,7 @@ class Bird(pg.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = xy
         self.speed = 10
-        self.high_speed = 20 #feature1
+        self.high_speed = 20  # feature1
         self.high = False
 
     def change_img(self, num: int, screen: pg.Surface):
@@ -129,7 +129,7 @@ class Bomb(pg.sprite.Sprite):
         self.image.set_colorkey((0, 0, 0))
         self.rect = self.image.get_rect()
         # 爆弾を投下するemyから見た攻撃対象のbirdの方向を計算
-        self.vx, self.vy = calc_orientation(emy.rect, bird.rect)  
+        self.vx, self.vy = calc_orientation(emy.rect, bird.rect)
         self.rect.centerx = emy.rect.centerx
         self.rect.centery = emy.rect.centery+emy.rect.height/2
         self.speed = 6
@@ -149,7 +149,7 @@ class Beam(pg.sprite.Sprite):
     """
     ビームに関するクラス
     """
-    def __init__(self, bird: Bird, angle0 = 0):
+    def __init__(self, bird: Bird, angle0=0):
         """
         ビーム画像Surfaceを生成する
         引数 bird：ビームを放つこうかとん
@@ -185,8 +185,8 @@ class NeoBeam:
         
     def gen_beams(self) -> list[Beam]:
         return [Beam(self.bird, angle) for angle in range(-50, +51, int(100/(self.num-1)))]
-      
-   
+
+
 class Explosion(pg.sprite.Sprite):
     """
     爆発に関するクラス
@@ -243,6 +243,38 @@ class Enemy(pg.sprite.Sprite):
         self.rect.centery += self.vy
 
 
+class Boss(pg.sprite.Sprite):
+    """
+    ボスに関するクラス
+    """
+    imgs = [pg.transform.rotozoom(pg.image.load(f"fig/alien{i}.png"), 0, 3.0) for i in range(1, 4)]
+    
+    def __init__(self):
+        super().__init__()
+        self.image = random.choice(__class__.imgs)
+        self.rect = self.image.get_rect()
+        self.rect.center = random.randint(0, WIDTH), 0
+        self.vy = +6
+        self.bound = random.randint(50, HEIGHT/2)  # 停止位置
+        self.state = "down"  # 降下状態or停止状態
+        self.interval = 50  # 爆弾投下インターバル
+        self.hp = 5  # ボスのhp
+        
+    def update(self):
+        """
+        ボスを速度ベクトルself.vyに基づき移動（降下）させる
+        ランダムに決めた停止位置_boundまで降下したら，_stateを停止状態に変更する
+        引数 screen：画面Surface
+        """
+        if self.rect.centery > self.bound:
+            self.vy = 0
+            self.state = "stop"
+        self.rect.centery += self.vy
+        if self.state == "stop" and pg.time.get_ticks() % self.interval == 0:
+            return True  # 爆弾を連射する
+        return False  # 爆弾を連射しない
+
+
 class Score:
     """
     打ち落とした爆弾，敵機の数をスコアとして表示するクラス
@@ -261,22 +293,24 @@ class Score:
         self.image = self.font.render(f"Score: {self.value}", 0, self.color)
         screen.blit(self.image, self.rect)
 
+
 class Gravity(pg.sprite.Sprite):
     """
     画面全体を覆う重力場を発生させるクラス
     """
     def __init__(self, life):
         super().__init__()
-        self.image = pg.Surface((WIDTH,HEIGHT))
-        pg.draw.rect(self.image,(0,0,0),(0,0,WIDTH, HEIGHT))
+        self.image = pg.Surface((WIDTH, HEIGHT))
+        pg.draw.rect(self.image, (0, 0, 0), (0, 0, WIDTH, HEIGHT))
         self.image.set_alpha(255)
         self.life = life
-        self.rect=self.image.get_rect()
+        self.rect = self.image.get_rect()
 
     def update(self):
         self.life -= 1
         if self.life < 0:
             self.kill()           
+            
 
 class EMP(pg.sprite.Sprite):
     """
@@ -326,22 +360,20 @@ def main():
     neobeams = pg.sprite.Group()
     exps = pg.sprite.Group()
     emys = pg.sprite.Group()
-
+    bosses = pg.sprite.Group()
     emp = EMP(emys, bombs, screen)
-
     gravities = pg.sprite.Group()   # feature2
-
 
     tmr = 0
     clock = pg.time.Clock()
+    boss_tmr = 0
     while True:
         key_lst = pg.key.get_pressed()
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return 0
             if event.type == pg.KEYDOWN:
-                if key_lst[pg.K_LSHIFT]  and event.key == pg.K_SPACE:
-                    print("a")
+                if key_lst[pg.K_LSHIFT] and event.key == pg.K_SPACE:
                     beams.add(NeoBeam(bird, 5).gen_beams())
                 elif event.key == pg.K_SPACE:
                     beam = Beam(bird)
@@ -354,50 +386,75 @@ def main():
                     emp.activate()
         screen.blit(bg_img, [0, 0])
 
-        if tmr%200 == 0:  # 200フレームに1回，敵機を出現させる
+        if tmr % 200 == 0:  # 200フレームに1回，敵機を出現させる
             emys.add(Enemy())
 
+        boss_tmr += clock.get_time() / 1000  # 時間の経過を秒単位
+        if score.value <= 200 and boss_tmr >= 15: # scoreが200以下の時15秒ごとにボスを出現させる
+            bosses.add(Boss())
+            boss_tmr = 0  # ボス出現タイマーをリセット
+        elif 200 <= score.value <= 500 and boss_tmr >= 7: # scoreが200から500の時7秒ごとにボスを出現させる
+            bosses.add(Boss())
+            boss_tmr = 0
+        elif 500 <= score.value <= 900 and boss_tmr >= 3: #scoreが500から900の時3秒ごとにボスを出現させる
+            bosses.add(Boss())
+            boss_tmr = 0
+        elif 900 <= score.value and boss_tmr >= 1: #scoreが900以上の時1秒ごとにボスを出現させる
+            bosses.add(Boss())
+            boss_tmr = 0
+
         for emy in emys:
-            if emy.state == "stop" and tmr%emy.interval == 0:
+            if emy.state == "stop" and tmr % emy.interval == 0:
                 # 敵機が停止状態に入ったら，intervalに応じて爆弾投下
                 bombs.add(Bomb(emy, bird))
+
+        for boss in bosses:
+            if boss.update():
+                bombs.add(Bomb(boss, bird))  # ボスが爆弾を連射
 
         for emy in pg.sprite.groupcollide(emys, beams, True, True).keys():
             exps.add(Explosion(emy, 100))  # 爆発エフェクト
             score.value += 10  # 10点アップ
             bird.change_img(6, screen)  # こうかとん喜びエフェクト
 
+        for boss in pg.sprite.groupcollide(bosses, beams, False, True).keys():
+            boss.hp -= 1
+            if boss.hp <= 0:
+                exps.add(Explosion(boss, 100))
+                score.value += 100  # ボスを倒したら100点加算
+                bird.change_img(6, screen)
+                boss.kill()
+
         for bomb in pg.sprite.groupcollide(bombs, beams, True, True).keys():
-            exps.add(Explosion(bomb, 50))  # 爆発エフェクト
-            #score.value += 1  # 1点アップ
+            exps.add(Explosion(bomb, 50)) # 爆発エフェクト
 
         for bomb in pg.sprite.spritecollide(bird, bombs, True):
             if bomb.state == "active":
-                bird.change_img(8, screen) # こうかとん悲しみエフェクト
+                bird.change_img(8, screen)  # こうかとん悲しみエフェクト
                 score.update(screen)
                 pg.display.update()
                 time.sleep(2)
                 return
 
-            return        
-        if key_lst[pg.K_LSHIFT] and score.value > 200: #消費スコアが200より大きい
-            score.value -= 200  
-            gravities.add(Gravity(400))  
-        
+        if key_lst[pg.K_RSHIFT] and score.value > 200:  # 消費スコアが200より大きい
+            score.value -= 200
+            gravities.add(Gravity(400))
+
         for enemy in emys:
             for gravity in gravities:
                 if pg.sprite.collide_rect(enemy, gravity):
                     exps.add(Explosion(enemy, 100))
-                    enemy.kill()  
+                    enemy.kill()
 
-        
         gravities.update()
-        gravities.draw(screen)    
+        gravities.draw(screen)
         bird.update(key_lst, screen)
         beams.update()
         beams.draw(screen)
         emys.update()
         emys.draw(screen)
+        bosses.update()
+        bosses.draw(screen)
         bombs.update()
         bombs.draw(screen)
         exps.update()
